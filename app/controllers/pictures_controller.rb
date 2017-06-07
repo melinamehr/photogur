6ef480e49592d290f137715d407a181376d41583
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-07 14:11:37 -0400

    form for a new photo created

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,4 +8,15 @@ class PicturesController < ApplicationController
    @picture = Picture.find(params[:id])
  end
 
+ def new
+   @picture = Picture.new
+ end
+
+  def create
+  end
+
+  def create
+   render text: "Received POST request to '/pictures' with the data URL: #{params}"
+ end
+
 end
