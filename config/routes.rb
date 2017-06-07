905c8b45dc7d55ab7f3b9d6199deb83f579b9d4a
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-07 15:17:42 -0400

    fixed repeating images, app complete

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get 'pictures/:id/edit' => "pictures#edit"
   patch 'pictures/:id' => "pictures#update"
 
+  delete 'pictures/:id' => 'pictures#destroy'
+
+
 end
