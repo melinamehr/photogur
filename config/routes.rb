eb247dd9dc8c4a28ea9bb24f637f853337a72109
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-16 15:41:29 -0400

    adding new user works

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
 
   post 'pictures' => 'pictures#create'
+
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show'
 
   get 'pictures/:id/edit' => "pictures#edit"
+
   patch 'pictures/:id' => "pictures#update"
 
   delete 'pictures/:id' => 'pictures#destroy'
 
   get 'show_year/:id' => 'pictures#show_year'
+
+  get 'users' => 'users#new'
+  post 'users' => 'users#create'
 
 
 end
